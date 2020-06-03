3f2596445e9f36d35cee6f83f176a41a47bb98d2
alexey <elshin195@gmail.com>
2020-06-03 18:33:01 +0500

    Updates

--- precache-manifest.e579a4a8a476ff1d8e1d21060c529df4.js
+++ precache-manifest.e579a4a8a476ff1d8e1d21060c529df4.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a8575823db24e3044e309553b4715734",
+    "revision": "b51e6aced991e4fe1d6ca77b92698fc5",
     "url": "/TimeTraker/index.html"
   },
   {
-    "revision": "f00c27d7cbd2b2bb6895",
+    "revision": "0d3cddbeb118e8cd0eef",
     "url": "/TimeTraker/static/css/main.f59c328b.chunk.css"
   },
   {
-    "revision": "4aea7ea69a55461b0c6f",
-    "url": "/TimeTraker/static/js/2.b290e94b.chunk.js"
+    "revision": "d4d231fa0724d9e98f50",
+    "url": "/TimeTraker/static/js/2.2e0398d3.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/TimeTraker/static/js/2.b290e94b.chunk.js.LICENSE.txt"
+    "url": "/TimeTraker/static/js/2.2e0398d3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f00c27d7cbd2b2bb6895",
-    "url": "/TimeTraker/static/js/main.65eb58f7.chunk.js"
+    "revision": "0d3cddbeb118e8cd0eef",
+    "url": "/TimeTraker/static/js/main.bb382d1e.chunk.js"
   },
   {
     "revision": "cd585f4d7404d992b216",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TimeTraker/precache-manifest.44413a2eea0e7979989e82b84aa77215.js"
+  "/TimeTraker/precache-manifest.e579a4a8a476ff1d8e1d21060c529df4.js"
 );
 
 self.addEventListener('message', (event) => {
